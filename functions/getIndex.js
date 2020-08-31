ff833519c1f5882404af90327beaf42ccf0630b4
Quentin <quentin.dejean34@gmail.com>
2020-09-01 09:35:29 +1000

    Add IAM authorizer to access restaurants endpoint (#2)
    Co-authored-by: Quentin Dejean <quentin.dejean@cloudconformity.com>

--- functions/getIndex.js
+++ functions/getIndex.js
@@ -1,5 +1,25 @@
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
+const Mustache = require('mustache');
+const axios = require('axios');
+const { aws4Interceptor } = require("aws4-axios");
+
+const restaurantsAPIRoot = process.env.RESTAURANT_API;
+const region = process.env.REGION;
+
+const interceptor = aws4Interceptor({
+  region,
+  service: "execute-api",
+});
+
+const httpClient = axios.create({
+  baseURL: restaurantsAPIRoot,
+});
+
+httpClient.interceptors.request.use(interceptor);
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 
 let htmlTemplate;
 
@@ -11,8 +31,19 @@ const loadHtml = async () => {
   return htmlTemplate
 }
 
+const getRestaurants = async () => {
+  const results = await httpClient.get("/restaurants");
+  return results.data;
+}
+
 module.exports.handler = async event => {
-  const html = await loadHtml();
+  
+  const template = await loadHtml();
+
+  const restaurants = await getRestaurants();
+  const dayOfWeek = days[new Date().getDay()];
+  
+  const html = Mustache.render(template, { dayOfWeek, restaurants });
 
   const response = {
     statusCode: 200,
