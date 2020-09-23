a530e86ccdce5862c6aa482f7883d51492c15577
Quentin Dejean <quentin.dejean@cloudconformity.com>
2020-09-23 14:42:17 +1000

    Add ability to place an order

--- functions/getIndex.js
+++ functions/getIndex.js
@@ -1,10 +1,12 @@
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
-const Mustache = require('mustache');
-const axios = require('axios');
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const Mustache = require("mustache");
+const axios = require("axios");
 const { aws4Interceptor } = require("aws4-axios");
 
 const restaurantsAPIRoot = process.env.RESTAURANT_API;
+const placeOrderAPI = `${restaurantsAPIRoot}/order`;
+
 const region = process.env.REGION;
 const cognitoUserPoolId = process.env.COGNITO_USER_POOL_ID;
 const cognitoClientId = process.env.COGNITO_CLIENT_ID;
@@ -20,8 +22,15 @@ const httpClient = axios.create({
 
 httpClient.interceptors.request.use(interceptor);
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 let htmlTemplate;
 
@@ -30,33 +39,38 @@ const loadHtml = async () => {
     htmlTemplate = await fs.readFileAsync("static/index.html", "utf-8");
   }
 
-  return htmlTemplate
-}
+  return htmlTemplate;
+};
 
 const getRestaurants = async () => {
   const results = await httpClient.get("/restaurants");
   return results.data;
-}
+};
 
-module.exports.handler = async event => {
-  
+module.exports.handler = async (event) => {
   const template = await loadHtml();
 
   const restaurants = await getRestaurants();
   const dayOfWeek = days[new Date().getDay()];
   const view = {
-    dayOfWeek, restaurants, awsRegion: region, cognitoUserPoolId, cognitoClientId, searchUrl: `${restaurantsAPIRoot}/restaurants/search`
-  }
-  
+    dayOfWeek,
+    restaurants,
+    awsRegion: region,
+    cognitoUserPoolId,
+    cognitoClientId,
+    searchUrl: `${restaurantsAPIRoot}/restaurants/search`,
+    placeOrderUrl: placeOrderAPI,
+  };
+
   const html = Mustache.render(template, view);
 
   const response = {
     statusCode: 200,
     body: html,
     headers: {
-      'Content-Type': 'text/html chatset=UTF-8'
-    }
-  }
+      "Content-Type": "text/html chatset=UTF-8",
+    },
+  };
 
-  return response
+  return response;
 };
