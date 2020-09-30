fa892773e89fefb4ffe4e4f366055e9ec1b635bf
Quentin Dejean <quentin.dejean@cloudconformity.com>
2020-09-30 13:10:07 +1000

    Add ability to add an order

--- constants/order.js
+++ constants/order.js
@@ -1,4 +1,5 @@
 const ORDER_PLACED = "order_placed";
 const RESTAURANT_NOTIFIED = "restaurant_notified";
+const ORDER_ACCEPTED = "order_accepted";
 
-module.exports = { ORDER_PLACED, RESTAURANT_NOTIFIED };
+module.exports = { ORDER_ACCEPTED, ORDER_PLACED, RESTAURANT_NOTIFIED };
