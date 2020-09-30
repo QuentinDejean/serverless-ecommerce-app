2db928957c13d4f579d248a8645ba84c329b789d
Quentin <quentin.dejean34@gmail.com>
2020-09-30 14:57:24 +1000

    Add ability to notify user and fulfill order (#8)

--- constants/order.js
+++ constants/order.js
@@ -1,5 +1,13 @@
+const ORDER_ACCEPTED = "order_accepted";
+const ORDER_FULFILLED = "order_fulfilled";
 const ORDER_PLACED = "order_placed";
 const RESTAURANT_NOTIFIED = "restaurant_notified";
-const ORDER_ACCEPTED = "order_accepted";
+const USER_NOTIFIED = "user_notified";
 
-module.exports = { ORDER_ACCEPTED, ORDER_PLACED, RESTAURANT_NOTIFIED };
+module.exports = {
+  ORDER_ACCEPTED,
+  ORDER_FULFILLED,
+  ORDER_PLACED,
+  RESTAURANT_NOTIFIED,
+  USER_NOTIFIED,
+};
